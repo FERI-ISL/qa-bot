f0776e3859ae0fbd48ce2afeb65d20bc9fc5770b
Grega Vrbančič <grega.vrbancic@gmail.com>
2023-09-05 17:06:50 +0200

    Workshop starter prepared.

--- src/main/java/si/um/feri/vrbancic/sentence_similarity_example/services/QAEmbeddingService.java
+++ src/main/java/si/um/feri/vrbancic/sentence_similarity_example/services/QAEmbeddingService.java
@@ -1,37 +1,16 @@
 package si.um.feri.vrbancic.sentence_similarity_example.services;
 
-import java.util.Arrays;
-
 import org.springframework.stereotype.Service;
-
-import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory;
-import ai.djl.inference.Predictor;
-import ai.djl.repository.zoo.Criteria;
-import ai.djl.repository.zoo.ZooModel;
-import ai.djl.training.util.ProgressBar;
 
 @Service
 public class QAEmbeddingService {
     
-    private Criteria<String, float[]> criteria =
-                Criteria.builder()
-                        .setTypes(String.class, float[].class)
-                        .optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/all-MiniLM-L6-v2")
-                        //.optModelPath(Paths.get("path to model"))
-                        //.optEngine("OnnxRuntime")
-                        .optTranslatorFactory(new TextEmbeddingTranslatorFactory())
-                        .optProgress(new ProgressBar())
-                        .build();
+    // TODO: naložite model za vstavljanje (embedding) vprašanj in odgovorov
+    // https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
 
-    
     public float[] embedQuestion(String question) {
-        try (ZooModel<String, float[]> model = criteria.loadModel(); Predictor<String, float[]> predictor = model.newPredictor()) {
-            float[] embeddedQuestion = predictor.predict(question);
-            return embeddedQuestion;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        // TODO: implementirajte vstavljanje (embedding) vprašanja ter vrnite vektor
+        return null;
     }
 
 }
